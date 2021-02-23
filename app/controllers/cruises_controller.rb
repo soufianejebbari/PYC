7df0b2c62815c155723187d7aa9dd8a2c078e286
marc mizrahi <mizrahimarc@gmail.com>
2021-02-23 12:36:32 +0100

    pushed the pundit for the cruise

--- app/controllers/cruises_controller.rb
+++ app/controllers/cruises_controller.rb
@@ -6,14 +6,17 @@ class CruisesController < ApplicationController
   def show
     @cruise = Cruise.find(params[:id])
     @booking = Booking.new
+    authorize @cruise
   end
 
   def new
     @cruise = Cruise.new
+    authorize @cruise
   end
 
   def create
     @cruise = Cruise.new(cruise_params)
+    authorize @cruise
     if @cruise.save
       redirect_to cruise_path(@cruise)
     else
@@ -23,10 +26,12 @@ class CruisesController < ApplicationController
 
   def edit
     @cruise = Cruise.find(params[:id])
+    authorize @cruise
   end
 
   def update
     @cruise = Cruise.find(params[:id])
+    authorize @cruise
     if @cruise.update(cruise_params)
       redirect_to @cruise
     else
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-  devise_for :users
+  devise_for :users,
               path: '',
               path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile'}
   resources :users, only: :show
