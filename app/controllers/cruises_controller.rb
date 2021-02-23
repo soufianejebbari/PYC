1e29818ae10a1891ff999bbd4f519412ea068b88
marc mizrahi <mizrahimarc@gmail.com>
2021-02-23 11:53:57 +0100

    pushed the cruise controller

--- app/controllers/cruises_controller.rb
+++ app/controllers/cruises_controller.rb
@@ -1,5 +1,42 @@
 class CruisesController < ApplicationController
+  def index
+    @cruise = Cruise.all
+  end
+
+  def show
+    @cruise = Cruise.find(params[:id])
+    @booking = Booking.new
+  end
+
   def new
     @cruise = Cruise.new
+  end
+
+  def create
+    @cruise = Cruise.new(cruise_params)
+    if @cruise.save
+      redirect_to cruise_path(@cruise)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @cruise = Cruise.find(params[:id])
+  end
+
+  def update
+    @cruise = Cruise.find(params[:id])
+    if @cruise.update(cruise_params)
+      redirect_to @cruise
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def cruise_params
+    params.require(:cruise).permit(:start_date, :end_date, :price, :start_location, :end_location, :cruise_id)
   end
 end
