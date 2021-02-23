d189347cd318937ee16b67171a12bf4e905474a2
SoufianeJebbari <soufiane.jebbari@outlook.fr>
2021-02-23 17:46:33 +0100

    creating seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :boats, dependent: :destroy
+  has_many :boats
   has_many :reviews, dependent: :destroy
   has_many :bookings, dependent: :destroy
   has_many :messages, dependent: :destroy
