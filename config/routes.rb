8f399bd06a63c2eaddfcf7e2a3508111cc576eee
Agathe Saint-Frison <agathe.saintfrison@orange.fr>
2021-02-23 12:19:12 +0100

    Add : Dashboard to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  
+
   devise_for :users
               path: '',
               path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile'}
   resources :users, only: :show
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :cruises do
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   end
 
   resources :boats, only: [:new, :edit, :create, :update, :destroy]
+  get 'dashboard', to: 'dashboards#dashboard'
 
 end
 
