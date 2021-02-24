f61ffe87a5d65cc176194b16a5d5c715a102498f
Francois Fauvel <francoisfauvel@outlook.fr>
2021-02-24 12:26:33 +0100

    Models updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_161934) do
+ActiveRecord::Schema.define(version: 2021_02_24_111345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 2021_02_23_161934) do
     t.string "name"
     t.string "description"
     t.string "category"
-    t.string "activity"
     t.integer "capacity"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -46,6 +45,9 @@ ActiveRecord::Schema.define(version: 2021_02_23_161934) do
     t.bigint "boat_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.string "description"
+    t.integer "difficulty"
     t.index ["boat_id"], name: "index_cruises_on_boat_id"
   end
 
