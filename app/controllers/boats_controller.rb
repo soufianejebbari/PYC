cd16a75a0a9b6633fb30448a5a91da6c046ad96e
Francois Fauvel <francoisfauvel@outlook.fr>
2021-02-24 16:02:46 +0100

    redirection to user show create

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -27,7 +27,7 @@ class BoatsController < ApplicationController
     @boat = Boat.new(boat_params)
     @boat.user = current_user
     if @boat.save
-      redirect_to boat_path(@boat)
+      redirect_to user_path(current_user)
     else
       render :new
     end
