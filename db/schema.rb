a6bcbaa290f4b80e986347b277eaa16c726356a6
SoufianeJebbari <soufiane.jebbari@outlook.fr>
2021-02-24 16:07:40 +0100

    create location table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_24_114619) do
+ActiveRecord::Schema.define(version: 2021_02_24_143148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,14 @@ ActiveRecord::Schema.define(version: 2021_02_24_114619) do
     t.float "latitude"
     t.float "longitude"
     t.index ["boat_id"], name: "index_cruises_on_boat_id"
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string "name"
+    t.float "latitude"
+    t.float "longitude"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "messages", force: :cascade do |t|
