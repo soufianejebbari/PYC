508769e3eb1defa2a0de0bc9d5ebfcf1a8385a0d
marc mizrahi <mizrahimarc@gmail.com>
2021-02-24 12:54:13 +0100

    updated the model for the geocoding

--- app/models/cruise.rb
+++ app/models/cruise.rb
@@ -11,4 +11,7 @@ class Cruise < ApplicationRecord
   validates :start_location, presence: true
   validates :end_location, presence: true
   validates :difficulty, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
