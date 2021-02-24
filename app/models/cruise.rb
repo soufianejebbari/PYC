e347b39b72ec3ba0e610d8d21709d8e5251c662a
marc mizrahi <mizrahimarc@gmail.com>
2021-02-24 16:08:55 +0100

    geocode is mire or less done, need to add the markers and make it look good

--- app/models/cruise.rb
+++ app/models/cruise.rb
@@ -12,6 +12,6 @@ class Cruise < ApplicationRecord
   validates :end_location, presence: true
   validates :difficulty, presence: true
 
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
+  geocoded_by :start_location
+  after_validation :geocode, if: :will_save_change_to_start_location?
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,11 @@ require 'open-uri'
 require 'nokogiri'
 require 'faker'
 require "yaml"
+require 'date'
 
 puts "Destroying all instances"
-Cruise.destroy_all
 Boat.destroy_all
+Cruise.destroy_all
 Booking.destroy_all
 Review.destroy_all
 Message.destroy_all
@@ -20,7 +21,6 @@ boats = [{
   photo: "https://static1.clickandboat.com/v1/p/0lly6fazbt45f0pmeav1ysmoq36dexvv.big.jpg.gz",
   category: "Sun Odissey 349",
   capacity: 6,
-  activity: ["scuba diving", "paddle", "Canoe", "water skiing"].sample,
   user_id: 1
 },
 {
@@ -29,7 +29,6 @@ boats = [{
   photo: "https://static1.clickandboat.com/v1/p/7k56w3ksiells2bia9fp75q0wlym1w64.big.jpg.gz",
   category: "Lagoon 420",
   capacity: 8,
-  activity: ["scuba diving", "paddle", "Canoe", "water skiing"].sample,
   user_id: 1
 },
 {
@@ -38,7 +37,6 @@ boats = [{
   photo: "https://static1.clickandboat.com/v1/p/18hjmkt8vm6kdnyvsrf8zwh5gnwi12ax.big.jpg.gz",
   category: "Bahia 46",
   capacity: 8,
-  activity: ["scuba diving", "paddle", "Canoe", "water skiing"].sample,
   user_id: 1
 },
 {
@@ -47,7 +45,6 @@ boats = [{
   photo: "https://static1.clickandboat.com/v1/p/k1hi4mveo5i36s49i666gwb0e1uthpnf.big.jpg.gz",
   category: "Feeling 32",
   capacity: 4,
-  activity: ["scuba diving", "paddle", "Canoe", "water skiing"].sample,
   user_id: 1
 },
 {
@@ -56,7 +53,6 @@ boats = [{
   photo: "https://static1.clickandboat.com/v1/p/lfp29o37ywzht0zp6xq2flhfe6blf6hr.big.jpg.gz",
   category: "Dufour 45 Performance",
   capacity: 8,
-  activity: ["scuba diving", "paddle", "Canoe", "water skiing"].sample,
   user_id: 1
 },
 {
@@ -65,7 +61,6 @@ boats = [{
   photo: "https://static1.clickandboat.com/v1/p/ol31joo5zngi85kp9kpl5jx3aubbgata.big.jpg.gz",
   category: "Laboe 51",
   capacity: 6,
-  activity: ["scuba diving", "paddle", "Canoe", "water skiing"].sample,
   user_id: 1
 },
 {
@@ -74,7 +69,6 @@ boats = [{
   photo: "https://static1.clickandboat.com/v1/p/te2yJvd38oNcvPy53Bh5dxpQP6VsTR1d.big.jpg.gz",
   category: "Sense 55",
   capacity: 8,
-  activity: ["scuba diving", "paddle", "Canoe", "water skiing"].sample,
   user_id: 1
 },
 {
@@ -83,7 +77,6 @@ boats = [{
   photo: "https://static1.clickandboat.com/v1/p/1pd04n9y42m0k0mq9h2rcvvew9lmgfy8.big.jpg.gz",
   category: "Bali 4.1",
   capacity: 8,
-  activity: ["scuba diving", "paddle", "Canoe", "water skiing"].sample,
   user_id: 1
 },
 {
@@ -92,7 +85,6 @@ boats = [{
   photo: "https://static1.clickandboat.com/v1/p/0gZs8iCPiz5gPymwqDjlYZs9gfaqODGW.big.jpg.gz",
   category: "Fontaine Pajot 40 Lucia",
   capacity: 4,
-  activity: ["scuba diving", "paddle", "Canoe", "water skiing"].sample,
   user_id: 1
 },
 {
@@ -101,12 +93,8 @@ boats = [{
   photo: "https://static1.clickandboat.com/v1/p/5ie0mx2juawys96g0dzl898hbgnuo6p9.big.jpg.gz",
   category: "Sun-Fast 39",
   capacity: 6,
-  activity: ["scuba diving", "paddle", "Canoe", "water skiing"].sample,
   user_id: 1
-}
-]
-
-
+}]
 
 boats.each do |boat|
   boaty = Boat.new(boat)
@@ -116,23 +104,28 @@ boats.each do |boat|
   # boaty.photos.attach(boaty.photo)
 end
 
-# html_doc.css('div.envItem').each do |a|
+cruises = [{
+  name: "Nice trip all around Corsica",
+  description: Faker::Lorem.paragraph,
+  difficulty: 3,
+  start_location: "Porto Vecchio",
+  end_location: "Bonifacio",
+  price: rand(90..300),
+  start_date: Date.today,
+  end_date: Date.today+1,
+  boat: Boat.first,
+}]
 
-#   url_design = "https://www.clickandboat.com#{a.attribute('href')}"
-#   html_file_design = open(url_design).read
-#   html_doc_design = Nokogiri::HTML(html_file_design)
-
-  # image_url = html_doc_voilier.at('.card-img img').attribute('src').value
-
-  # name = {"Cash à l'eau", "Ode mer", "Patpau", "Lashepriz", "Nages et tais toi", "Almaco IV", "Croque le vent", "Altaîr", "Mangareva", "Vaîmiti"].sample
-
-  # description = loremipsum30.text
-
-  # category = ["monocoque", "catamaran"].sample
+cruises.each do |cruise|
+  cruisy = Cruise.new(cruise)
+  p cruisy
+  cruisy.valid?
+  cruisy.save!
+  # boaty.photos.attach(boaty.photo)
+end
 
   # activity = ["scuba diving", "paddle", "Canoe", "water skiing"].sample
 
-  # capacity = [4, 6, 8].sample
 
   # price = rand(10..40)
 
@@ -157,4 +150,3 @@ puts "#{Boat.all.length} boats created"
 
 # board image to do after cloudinary setup
 # image_url = html_doc.at('.image-inset img').attribute('src').value
-
