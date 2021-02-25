10d54c3984d16a5210dadf7297981f4a3bbc44f1
SoufianeJebbari <soufiane.jebbari@outlook.fr>
2021-02-25 11:55:05 +0100

    update and correct start and end location

--- db/migrate/20210222154211_create_cruises.rb
+++ db/migrate/20210222154211_create_cruises.rb
@@ -4,9 +4,9 @@ class CreateCruises < ActiveRecord::Migration[6.0]
       t.date :start_date
       t.date :end_date
       t.integer :price
-      t.string :start_location
-      t.string :end_location
       t.references :boat, null: false, foreign_key: true
+      t.references :start_location, foreign_key: { to_table: :locations }, index: true
+      t.references :end_location, foreign_key: { to_table: :locations }, index: true
 
       t.timestamps
     end
